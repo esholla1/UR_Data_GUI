419261c4b4ad699334dc855abdc08ff9bfe6bd0e
esholla1 <44717893+esholla1@users.noreply.github.com>
2019-11-26 13:20:06 -0500

    Update gui_trial.py

--- gui_trial.py
+++ gui_trial.py
@@ -48,34 +48,73 @@ Created on Tue Nov 19 15:56:05 2019
 #!/usr/bin/env python3
 
 from tkinter import *
+import socket
 
+# ************* Socket Programming ***********************
+
+
+
+#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+##as e server we need to bind the socket to the server port
+#s.bind((socket.gethostname(), 12345))
+##our queue will be 5 PCs
+#s.listen(5)
+#clientSocket, address = s.accept()
+#print(f"Connection from {address} has been established")
+#
+#stop_word = "quit"
+#stop_word = stop_word + '\r\n'
+#f = open("client_text.txt", "w")
+#full_msg = ''
+#
+#while True:
+#    msg = clientSocket.recv(1024)
+#    print(msg.decode("utf-8"))
+#    full_msg = msg.decode("utf-8")
+#    if full_msg == stop_word:
+#        clientSocket.close()
+#        f.close()
+#    f.write(full_msg)
+
+
+
+# ************** Creation of GUI ************************
 root = Tk()
 root.title("UR-Gui")
 root.geometry("190x200")
 
+def close_window(): #when clicking the Quit button this fcn will get activated
+    root.destroy()
+
+ip_string_var = StringVar()
+ip_string_var.set('271.0.0.50')
+port_string_var = StringVar()
+port_string_var.set('12345')
+msg_string_var = StringVar()
+msg_string_var.set('Hello')
 
 
-
-button2 = Button(text="Connect", fg="green")
-button2.pack()
 
 YNA = Label(root, text="Yazaki North America", bg="red", fg="white")
 YNA.pack(fill=X)
-
+button2 = Button(text="Connected", fg="white", bg = "green")
+button2.pack()
 ip = Label(root, text="IP Address")
 ip.pack()
-entry1 = Entry(root)
+entry1 = Entry(root, textvariable = ip_string_var)
 entry1.pack()
 port = Label(root, text="Port Number")
-entry2 = Entry(root)
+entry2 = Entry(root, textvariable = port_string_var)
 port.pack()
 entry2.pack()
 message = Label(root, text = "Message")
 message.pack()
-entry3 = Entry(root)
+entry3 = Entry(root, textvariable = msg_string_var)
 entry3.pack()
-
-button1 = Button(text="Quit", fg="red")
+button1 = Button(text="Quit",command = close_window, fg="red")
 button1.pack()
-root.mainloop()
+#text = Text(root)
+#text.insert(INSERT, "HELLO")
+#text.pack()
 
+root.mainloop()
