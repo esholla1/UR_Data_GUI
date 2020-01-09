9bbe334f993baa0de89e15811b32db58e18ce23d
esholla1 <44717893+esholla1@users.noreply.github.com>
2020-01-09 07:16:08 -0800

    Update server_gui.py

--- server_gui.py
+++ server_gui.py
@@ -1,38 +1,157 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  3 14:57:11 2019
+@author: AMZ
+"""
+
+
+# -*- coding: utf-8 -*-
+
+"""
+@author: Elis Sholla
+"""
+
+
+
+from tkinter import *
+from PIL import Image, ImageTk
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s.bind(("121.0.0.50", 21))
-s.bind((socket.gethostname(), 12345))
-#s.close()
-#clientSocket.close()
-#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#as e server we need to bind the socket to the server port
-#s.bind((socket.gethostname(), 12345))
-#s.bind(("121.0.0.50", 21))
-#our queue will be 5 PCs
-s.listen(5)
-clientSocket, address = s.accept()
-print(f"Connection from {address} has been established")
+from functools import partial
+
+def close_window(s): #when clicking the Quit button this fcn will get activated
+    s.close()
+    root.destroy()
 
 stop_word = "quit"
 stop_word = stop_word + '\r\n'
-f = open("Yazaki.txt", "w")
-#full_msg = ''
-full_msg = '0'
+globalfull_msg = '0'
+full_msg = "0"
+Status = "0"
 
-while True:
-    msg = clientSocket.recv(1024)
-    print(msg.decode("utf-8"))
-    full_msg = msg.decode("utf-8")
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Creation of 
+#s.bind(("121.0.0.20", 21))
+s.bind((socket.gethostname(), 12345))
+s.listen(5)
 
+
+
+def socket_connection():
+    global Status
+    global clientSocket
+    global full_msg
+    
+    
+    print("socket_connection 1")
+    if  Status == "0":
+        clientSocket, address = s.accept()
+        print("socket_connection 2")
+        print(address[0])
+        print(address[1])
+        ip_string_var.set(address[0])
+        port_string_var.set(address[1])
+        connected_sv.set("Connected")
+        Status = "1"
+#       
+        
+        
+    else:
+        print("Espera datos por segunda vez")
+        msg = clientSocket.recv(1024)
+        print(msg.decode("utf-8"))
+        full_msg = msg.decode("utf-8")
+        print("Full_msg 2",full_msg)
+        
+        if full_msg == "":
+            print("Desconectado") 
+            connected_sv.set("Not Connected")
+            Status = "0"
+   
+    
     if full_msg == stop_word:
         clientSocket.close()
         f.close()
-        break
-
+        #break
+    
     if full_msg != '0':
         f = open("Yazaki.txt", "a")
         full_msg = full_msg.replace('\r\n', '')
+        msg_string_var.set(full_msg)
         f.write(full_msg)
+        
+        if full_msg[0] == "p":
+            im = Image.open("panda.jpg")
+            im = im.resize((250, 250), Image.ANTIALIAS)
+            smiley_img = ImageTk.PhotoImage(im)
+
+            smiley_lbl.configure(image=smiley_img)
+            smiley_lbl.image=smiley_img
+        
         f.close()
+        
+#        if full_msg == "hi":
+
+        
+        
         full_msg = '0'
+                
+
+    root.after(5000, socket_connection)
+
+
+
+# ************** Creation of GUI ************************
+
+
+root = Tk()
+root.title("UR-Gui")
+root.geometry("500x500")
+address = (0, 0)
+ip = address[0]
+port = address[1]
+
+print(ip, type(ip)) #ip type --> string
+
+print(f"Connection from {address} has been established")
+
+ip_string_var = StringVar()
+ip_string_var.set(ip)
+port_string_var = StringVar()   
+port_string_var.set(port)
+msg_string_var = StringVar()
+msg_string_var.set('Hello')
+connected_sv = StringVar()
+connected_sv.set("Not Connected")
+YNA = Label(root, text="Yazaki North America", bg="red", fg="white")
+YNA.pack(fill=X)
+button2 = Button(textvariable=connected_sv, fg="white", bg = "green")
+button2.pack()
+ip = Label(root, text="IP Address")
+ip.pack()
+entry1 = Entry(root, textvariable = ip_string_var)
+entry1.pack()
+port = Label(root, text="Port Number")
+entry2 = Entry(root, textvariable = port_string_var)
+port.pack()
+entry2.pack()
+message = Label(root, text = "Message")
+message.pack()
+entry3 = Entry(root, textvariable = msg_string_var)
+entry3.pack()
+
+button1 = Button(text="Quit",command = partial(close_window, s), fg="red")
+button1.pack()
+
+im = Image.open("pic.jpg")
+im = im.resize((250, 250), Image.ANTIALIAS)
+smiley_img = ImageTk.PhotoImage(im)
+
+smiley_lbl = Label(root, image=smiley_img)
+smiley_lbl.pack(side = "bottom", fill = "both", expand="yes")
+
+
+
+#clientSocket, address = s.accept()
+
+root.after(5000, socket_connection)
+root.mainloop()
