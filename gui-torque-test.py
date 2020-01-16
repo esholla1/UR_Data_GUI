43100ef388c6f3f3ce3e6becdd09365bbcc20438
esholla1 <44717893+esholla1@users.noreply.github.com>
2020-01-16 07:11:40 -0800

    Update gui-torque-test.py

--- gui-torque-test.py
+++ gui-torque-test.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Thu Jan  9 11:09:55 2020
 
@@ -18,6 +17,20 @@ from PIL import Image, ImageTk
 def close_window(s): #when clicking the Quit button this fcn will get activated
    #s.close()
     root.destroy()
+
+def change_color():
+    pn1_cb_sv.set('Checked')
+    pn2_cb_sv.set('Checked')
+    pn3_cb_sv.set('Checked')
+
+    l.config(bg='green')
+    m.config(bg = 'green')
+    r.config(bg='green')
+    button2.config(bg='green')
+    root.after(500, change_color)
+    connected_sv.set("Connected")
+    
+
 
 s = '121.0.0.20'
 root = Tk()
@@ -44,7 +57,7 @@ connected_sv = StringVar()
 connected_sv.set("Not Connected")
 YNA = Label(root, text="Yazaki North America", bg="red", fg="white")
 YNA.pack(fill=X)
-button2 = Button(textvariable=connected_sv, fg="white", bg = "green")
+button2 = Button(textvariable=connected_sv, fg="white", bg = "red")
 button2.pack()
 ip = Label(root, text="IP Address")
 ip.pack()
@@ -63,25 +76,64 @@ button1 = Button(text="Quit",command = partial(close_window, s), fg="red")
 button1.pack()
 
 
-###### Adding 3 side by side Images#####################
+####################### Adding 3 side by side Image  #####################
+
+pn1_sv = StringVar()
+pn1_sv.set('PartNumber1')
+pn2_sv = StringVar()
+pn2_sv.set('PartNumber2')
+pn3_sv = StringVar()
+pn3_sv.set('PartNumber3')
+
+
+frame1 = Frame(root, relief = RAISED, borderwidth = 2)
+frame1.pack(fill = X)
+l = Label(frame1, textvariable=pn1_sv, bg = "orange") #left
+l.pack(side = LEFT, padx = 0)
+m = Label(frame1, textvariable= pn2_sv, bg = "orange") #middle
+m.pack(side = LEFT, padx = 425)
+r = Label(frame1, textvariable=pn3_sv, bg = "orange")  #right
+r.pack(side = LEFT, padx =0) 
+
+
+frame2 = Frame(root, relief=RAISED, borderwidth = 2)
+frame2.pack(fill = X)
+
 im = Image.open("minion.jpg")
-im = im.resize((450, 450), Image.ANTIALIAS)
+im = im.resize((450, 400), Image.ANTIALIAS)
 img = ImageTk.PhotoImage(im)
-img_lbl = Label(root, image=img)
+img_lbl = Label(frame2, image=img)
 img_lbl.pack(side=LEFT)
 
-
-
 im2 = Image.open("panda.jpg")
-im2 = im2.resize((450, 450), Image.ANTIALIAS)
+im2 = im2.resize((450, 400), Image.ANTIALIAS)
 img2 = ImageTk.PhotoImage(im2)
-img2_lbl = Label(root, image=img2)
+img2_lbl = Label(frame2, image=img2)
 img2_lbl.pack(side=LEFT)
 
 im3 = Image.open("pic.jpg")
-im3 = im3.resize((450, 450), Image.ANTIALIAS)
+im3 = im3.resize((450, 400), Image.ANTIALIAS)
 img3 = ImageTk.PhotoImage(im3)
-img3_lbl = Label(root, image=img3)
+img3_lbl = Label(frame2, image=img3)
 img3_lbl.pack(side=LEFT)
 
+
+pn1_cb_sv = StringVar()  #part number 1 check box string variable
+pn1_cb_sv.set('Not Checked')
+pn2_cb_sv = StringVar()
+pn2_cb_sv.set('Not Checked')
+pn3_cb_sv = StringVar()
+pn3_cb_sv.set('Not Checked')
+
+
+frame3 = Frame(root, relief = RAISED, borderwidth = 2)
+frame3.pack(fill = X)
+l = Label(frame3, textvariable=pn1_cb_sv, bg = "red", fg = "white")
+l.pack(side = LEFT, padx = 0)
+m = Label(frame3, textvariable=pn2_cb_sv, bg = "red", fg = "white")
+m.pack(side = LEFT, padx = 425)
+r = Label(frame3, textvariable=pn3_cb_sv, bg = "red", fg = "white")
+r.pack(side = LEFT, padx =0)
+
+root.after(5000, change_color)
 root.mainloop()
